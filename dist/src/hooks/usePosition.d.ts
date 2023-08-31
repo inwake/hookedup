05648408df4a52308905ee4f9d4db0854c2540ff
Vitaliy <vitaliy@bybeapp.com>
2023-08-31 20:05:26 +0200

    Fix TS issues in usePosition

--- dist/src/hooks/usePosition.d.ts
+++ dist/src/hooks/usePosition.d.ts
@@ -1,9 +1,28 @@
-export default function usePosition({ continuous }: {
-    continuous?: boolean | undefined;
-}): {
-    position: null;
-    motion: null;
-    error: null;
+interface PositionHookProps {
+    streamPosition?: boolean;
+}
+interface Position {
+    latitude: number;
+    longitude: number;
+    accuracy: number;
+    altitude: number;
+    altitudeAccuracy: number;
+    timestamp: number;
+}
+interface Motion {
+    speed: number;
+    heading: number;
+}
+interface PositionError {
+    message: string;
+    status: string;
+    code: number;
+}
+export default function usePosition({ streamPosition }: PositionHookProps): {
+    position: Position | null;
+    motion: Motion | null;
+    error: PositionError | undefined;
     ready: boolean;
     getPosition: () => Promise<unknown>;
 };
+export {};
--- src/hooks/usePosition.tsx
+++ src/hooks/usePosition.tsx
@@ -1,79 +1,107 @@
 import {useState, useEffect} from 'react'
 import Geolocation from '@react-native-community/geolocation'
 
+interface PositionHookProps {
+	streamPosition?: boolean
+}
+
+interface Position {
+	latitude: number
+	longitude: number
+	accuracy: number
+	altitude: number
+	altitudeAccuracy: number
+	timestamp: number
+}
+
+interface Motion {
+	speed: number
+	heading: number
+}
+
+interface PositionError {
+	message: string
+	status: string
+	code: number
+}
+
 const config = {skipPermissionRequests: false,
 	enableBackgroundLocationUpdates: false,
 	authorizationLevel: 'whenInUse',
 	locationProvider: 'auto'}
 
 const getOptions = {enableHighAccuracy: true,
-	timeout: 10000,
-	maximumAge: 10000}
+	timeout: 10000, maximumAge: 10000}
 
 const watchOptions = {enableHighAccuracy: true,
-	interval: 2500,
-	fastestInterval: 1000,
-	distanceFilter: 25,
-	timeout: 10000,
-	maximumAge: 10000}
+	interval: 2500, fastestInterval: 1000,
+	timeout: 10000, maximumAge: 10000,
+	distanceFilter: 25}
 
-export default function usePosition({continuous=false}) {
-	const [position, setPosition] = useState(null)
-	const [motion, setMotion] = useState(null)
-	const [error, setError] = useState(null)
+export default function usePosition({streamPosition=false}: PositionHookProps) {
+	const [position, setPosition] = useState<Position | null>(null)
+	const [motion, setMotion] = useState<Motion | null>(null)
+	const [error, setError] = useState<PositionError>()
 	const [ready, setReady] = useState(false)
 
 	useEffect(function() {
-		// @ts-ignore
-		Geolocation.setRNConfiguration(config)
+		(Geolocation as any)
+			.setRNConfiguration(config)
 	}, [])
 
 	useEffect(function() {
-		if (!continuous) return
-		const id = Geolocation
-			.watchPosition(onPositionUpdate,
-				onUpdateError,
-				watchOptions)
-		return function() {
+		let id
+
+		if (streamPosition) setupPositionStream()
+		else if (!position) getPosition()
+
+		function setupPositionStream() {
+			if (streamPosition) {
+				id = Geolocation
+					.watchPosition(onPositionUpdate,
+						onUpdateError,
+						watchOptions)}}
+
+		function cleanUpPositionStream() {
 			Geolocation.clearWatch(id)}
-	})
 
-	function onPositionUpdate(data) {
+		return cleanUpPositionStream
+	}, [streamPosition])
+
+	function onPositionUpdate(data: any): Position {
 		const {coords, timestamp} = data
-		const motionData = {speed: coords.speed, heading: coords.heading}
-		const positionData = {latitude: coords.latitude,
-			longitude: coords.longitude,
-			accuracy: coords.accuracy,
-			altitude: coords.altitude,
-			altitudeAccuracy: coords.altitudeAccuracy,
-			timestamp: timestamp}
-
-		// Set state
-		// @ts-ignore
+		const motionData = {speed: coords.speed,
+			heading: coords.heading}
+		const positionData =
+			{latitude: coords.latitude,
+				longitude: coords.longitude,
+				accuracy: coords.accuracy,
+				altitude: coords.altitude,
+				altitudeAccuracy: coords
+					.altitudeAccuracy,
+				timestamp: timestamp}
 		setPosition(positionData)
-		// @ts-ignore
 		setMotion(motionData)
 		setReady(true)
 
-		// Return the processed data
 		return positionData}
 
 	function onUpdateError({code, message,
 		PERMISSION_DENIED: denied,
 		POSITION_UNAVAILABLE: unavailable,
-		TIMEOUT: timeout}) {
+		TIMEOUT: timeout}: any) {
 		const codeMap = {denied,
 			unavailable,
 			timeout}
 		const status = codeMap[code]
-		// @ts-ignore
 		setError({message, status, code})
 		setReady(true)}
 
 	function getPosition() {
 		return new Promise(function(resolve, reject) {
 			function success(positionData) {
-				const resolvedData = onPositionUpdate(positionData)
+				const resolvedData
+					= onPositionUpdate(positionData)
 				resolve(resolvedData)}
 			function failure(error) {
 				onUpdateError(error)
